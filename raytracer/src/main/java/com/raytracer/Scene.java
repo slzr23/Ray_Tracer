df30b392843ab58ed787317f1f20860837d099fd
Tulipe <leonce.duquerroy@etu.univ-nantes.fr>
2025-11-17 22:43:39 +0100

    ajout d'ambient, lights et shapes au constructeur de la scene

--- raytracer/src/main/java/com/raytracer/Scene.java
+++ raytracer/src/main/java/com/raytracer/Scene.java
@@ -1,16 +1,25 @@
 package com.raytracer;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Scene {
     private int width;
     private int height;
     private String outputFile = "output.png";
     private Camera camera;
+    private Color ambient = new Color();
+    private List<Light> lights = new ArrayList<>();
+    private List<Shape> shapes = new ArrayList<>();
 
-    public Scene(int width, int height, String outputFile, Camera camera) {
+    public Scene(int width, int height, String outputFile, Camera camera, Color ambient, List<Light> lights, List<Shape> shapes) {
         this.width = width;
         this.height = height;
         this.outputFile = outputFile;
         this.camera = camera;
+        this.ambient = ambient;
+        this.lights = lights;
+        this.shapes = shapes;
     }
 
     public int getWidth() {
