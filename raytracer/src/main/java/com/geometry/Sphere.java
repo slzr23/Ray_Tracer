b4a50596a0ebdf68d1ef2adef95d0307fdb241f7
Tulipe <leonce.duquerroy@etu.univ-nantes.fr>
2025-11-18 10:56:05 +0100

    ajout equals Sphere et Camera

--- raytracer/src/main/java/com/geometry/Sphere.java
+++ raytracer/src/main/java/com/geometry/Sphere.java
@@ -33,4 +33,33 @@ public class Sphere implements Shape {
         return radius;
     }
 
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Sphere other = (Sphere) obj;
+        if (center == null) {
+            if (other.center != null)
+                return false;
+        } else if (!center.equals(other.center))
+            return false;
+        if (Double.doubleToLongBits(radius) != Double.doubleToLongBits(other.radius))
+            return false;
+        if (diffuse == null) {
+            if (other.diffuse != null)
+                return false;
+        } else if (!diffuse.equals(other.diffuse))
+            return false;
+        if (specular == null) {
+            if (other.specular != null)
+                return false;
+        } else if (!specular.equals(other.specular))
+            return false;
+        return true;
+    }
+
 }
--- raytracer/src/main/java/com/raytracer/camera.java
+++ raytracer/src/main/java/com/raytracer/camera.java
@@ -31,4 +31,33 @@ public class Camera {
         return fov;
     }
 
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Camera other = (Camera) obj;
+        if (position == null) {
+            if (other.position != null)
+                return false;
+        } else if (!position.equals(other.position))
+            return false;
+        if (lookAt == null) {
+            if (other.lookAt != null)
+                return false;
+        } else if (!lookAt.equals(other.lookAt))
+            return false;
+        if (up == null) {
+            if (other.up != null)
+                return false;
+        } else if (!up.equals(other.up))
+            return false;
+        if (Double.doubleToLongBits(fov) != Double.doubleToLongBits(other.fov))
+            return false;
+        return true;
+    }
+
 }
